621390da1cd51592dc91dc11c184d24ecc06f47e
luciferwqy <190873411@163.com>
2017-08-11 15:43:55 +0800

    test

--- ordercenter/src/main/java/com/zm/order/bussiness/controller/OrderController.java
+++ ordercenter/src/main/java/com/zm/order/bussiness/controller/OrderController.java
@@ -1,26 +1,41 @@
 package com.zm.order.bussiness.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.zm.order.constants.ConfigConstants;
+import com.zm.order.pojo.OrderInfo;
 import com.zm.order.pojo.ResultPojo;
 
-/**  
- * ClassName: OrderController <br/>  
- * Function: TODO ADD FUNCTION. <br/>   
- * date: Aug 11, 2017 11:27:08 AM <br/>  
- *  
- * @author wqy  
- * @version   
- * @since JDK 1.7  
+/**
+ * ClassName: OrderController <br/>
+ * Function: TODO ADD FUNCTION. <br/>
+ * date: Aug 11, 2017 11:27:08 AM <br/>
+ * 
+ * @author wqy
+ * @version
+ * @since JDK 1.7
  */
 
 @RestController
 public class OrderController {
 
-	
-	public ResultPojo createOrder(){
-		ResultPojo result = new ResultPojo();
+	@RequestMapping(value = "{version}/order/creative", method = RequestMethod.POST)
+	public ResultPojo createOrder(@PathVariable("version") Double version, OrderInfo orderInfo, HttpServletRequest req,
+			HttpServletResponse res) {
 		
+		ResultPojo result = new ResultPojo();
+		//设置允许跨域请求
+		res.setHeader(ConfigConstants.CROSS_DOMAIN, ConfigConstants.DOMAIN_NAME);
+		
+		if (ConfigConstants.FIRST_VERSION.equals(version)) {
+			
+		}
 		return result;
 	}
 }
---
+++ ordercenter/src/main/java/com/zm/order/bussiness/dao/OrderMapper.java
@@ -0,0 +1,17 @@
+package com.zm.order.bussiness.dao;
+
+import com.zm.order.pojo.OrderInfo;
+
+/**  
+ * ClassName: OrderMapper <br/>  
+ * Function: TODO ADD FUNCTION. <br/>   
+ * date: Aug 11, 2017 3:40:13 PM <br/>  
+ *  
+ * @author wqy  
+ * @version   
+ * @since JDK 1.7  
+ */
+public interface OrderMapper {
+
+	void saveOrder(OrderInfo info);
+}
