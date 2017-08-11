355ff047684bbe82e410cf04cb00edd362d6ceb0
luciferwqy <190873411@163.com>
2017-08-11 13:57:00 +0800

    test

---
+++ ordercenter/src/main/java/com/zm/order/bussiness/controller/OrderController.java
@@ -0,0 +1,26 @@
+package com.zm.order.bussiness.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.zm.order.pojo.ResultPojo;
+
+/**  
+ * ClassName: OrderController <br/>  
+ * Function: TODO ADD FUNCTION. <br/>   
+ * date: Aug 11, 2017 11:27:08 AM <br/>  
+ *  
+ * @author wqy  
+ * @version   
+ * @since JDK 1.7  
+ */
+
+@RestController
+public class OrderController {
+
+	
+	public ResultPojo createOrder(){
+		ResultPojo result = new ResultPojo();
+		
+		return result;
+	}
+}
---
+++ ordercenter/src/main/java/com/zm/order/pojo/ResultPojo.java
@@ -0,0 +1,45 @@
+package com.zm.order.pojo;
+
+
+/**  
+ * ClassName: ResultPojo <br/>  
+ * Function: 返回统一对象. <br/>   
+ * date: Aug 11, 2017 1:55:04 PM <br/>  
+ *  
+ * @author wqy  
+ * @version   
+ * @since JDK 1.7  
+ */
+public class ResultPojo {
+
+	private String errorCode;
+	private String errorMsg;
+	private boolean success;
+	private Object obj;
+	public String getErrorCode() {
+		return errorCode;
+	}
+	public void setErrorCode(String errorCode) {
+		this.errorCode = errorCode;
+	}
+	public String getErrorMsg() {
+		return errorMsg;
+	}
+	public void setErrorMsg(String errorMsg) {
+		this.errorMsg = errorMsg;
+	}
+	public boolean isSuccess() {
+		return success;
+	}
+	public void setSuccess(boolean success) {
+		this.success = success;
+	}
+	public Object getObj() {
+		return obj;
+	}
+	public void setObj(Object obj) {
+		this.obj = obj;
+	}
+	
+	
+}
