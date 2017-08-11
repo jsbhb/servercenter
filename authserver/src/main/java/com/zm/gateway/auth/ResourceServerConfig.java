6eb5827b3bf28c7e7d08003becf895724a287804
jsbhb <306494983@qq.com>
2017-08-11 09:58:36 +0800

    init-2

--- authserver/src/main/java/com/zm/gateway/auth/ResourceServerConfig.java
+++ authserver/src/main/java/com/zm/gateway/auth/ResourceServerConfig.java
@@ -25,9 +25,9 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().exceptionHandling()
-				.authenticationEntryPoint(
-						(request, response, authException) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED))
-				.and().authorizeRequests().anyRequest().authenticated().and().httpBasic();
+//		http.csrf().disable().exceptionHandling()
+//				.authenticationEntryPoint(
+//						(request, response, authException) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED))
+//				.and().authorizeRequests().anyRequest().authenticated().and().httpBasic();
 	}
 }
--- ordercenter/src/main/java/com/zm/order/config/ResourceServerConfig.java
+++ ordercenter/src/main/java/com/zm/order/config/ResourceServerConfig.java
@@ -17,15 +17,16 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Created by wangyunfei on 2017/6/12.
  */
-@Configuration
-@EnableResourceServer
+//@Configuration
+//@EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().exceptionHandling()
-				.authenticationEntryPoint(
-						(request, response, authException) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED))
-				.and().authorizeRequests().anyRequest().authenticated().and().httpBasic();
+		// http.csrf().disable().exceptionHandling()
+		// .authenticationEntryPoint(
+		// (request, response, authException) ->
+		// response.sendError(HttpServletResponse.SC_UNAUTHORIZED))
+		// .and().authorizeRequests().anyRequest().authenticated().and().httpBasic();
 	}
 }
