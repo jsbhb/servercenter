c14ed39d09686126b056441909769195b33a534b
luciferwqy <190873411@163.com>
2017-08-11 16:20:07 +0800

    test

--- ordercenter/src/main/java/com/zm/order/bussiness/service/impl/OrderServiceImpl.java
+++ ordercenter/src/main/java/com/zm/order/bussiness/service/impl/OrderServiceImpl.java
@@ -1,5 +1,28 @@
 package com.zm.order.bussiness.service.impl;
 
-public class OrderServiceImpl {
+import javax.annotation.Resource;
 
+import com.zm.order.bussiness.dao.OrderMapper;
+import com.zm.order.bussiness.service.OrderService;
+import com.zm.order.pojo.OrderInfo;
+
+/**  
+ * ClassName: OrderServiceImpl <br/>  
+ * Function: TODO ADD FUNCTION. <br/>   
+ * date: Aug 11, 2017 3:54:27 PM <br/>  
+ *  
+ * @author wqy  
+ * @version   
+ * @since JDK 1.7  
+ */
+public class OrderServiceImpl implements OrderService{
+
+	@Resource
+	OrderMapper orderMapper;
+
+	@Override
+	public void saveOrder(OrderInfo info) {
+		
+		orderMapper.saveOrder(info);
+	}
 }
