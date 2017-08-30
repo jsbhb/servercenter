fc5d2cafb5951f4aea063b0c5f6b770897bd0e59
jsbhb <306494983@qq.com>
2017-08-30 09:35:03 +0800

    update auth function

--- authserver/src/main/java/com/zm/gateway/service/impl/AuthServiceImpl.java
+++ authserver/src/main/java/com/zm/gateway/service/impl/AuthServiceImpl.java
@@ -8,13 +8,13 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.zm.gateway.common.JWTUtil;
 import com.zm.gateway.mapper.UserMapper;
+import com.zm.gateway.model.SecurityUserDetail;
+import com.zm.gateway.model.SecurityUserFactory;
 import com.zm.gateway.model.UserInfo;
 import com.zm.gateway.service.AuthService;
 
@@ -32,25 +32,21 @@ import com.zm.gateway.service.AuthService;
 @Service
 public class AuthServiceImpl implements AuthService {
 
-	private AuthenticationManager authenticationManager;
-	private UserDetailsService userDetailsService;
 	private UserMapper userMapper;
 
 	@Value("${jwt.tokenHead}")
 	private String tokenHead;
 
 	@Autowired
-	public AuthServiceImpl(AuthenticationManager authenticationManager, UserDetailsService userDetailsService,
-			UserMapper userMapper) {
-		this.authenticationManager = authenticationManager;
-		this.userDetailsService = userDetailsService;
+	public AuthServiceImpl(UserMapper userMapper) {
 		this.userMapper = userMapper;
 	}
 
+	@Transactional
 	@Override
-	public String register(UserInfo userInfo) {
+	public SecurityUserDetail register(UserInfo userInfo) {
 
-		final String userName = userInfo.getUserName();
+		String userName = userInfo.getUserName();
 		if (userMapper.getUserByName(userName) != null) {
 			return null;
 		}
@@ -61,10 +57,14 @@ public class AuthServiceImpl implements AuthService {
 		userInfo.setAuthorities(asList("ROLE_USER"));
 		userMapper.insert(userInfo);
 
+		SecurityUserDetail userDetail = SecurityUserFactory.create(userInfo);
+
 		Map<String, Object> claim = new HashMap<String, Object>();
 		claim.put(JWTUtil.PASSWORD, userInfo.getPassword());
 		claim.put(JWTUtil.USER_NAME, userInfo.getUserName());
-		return JWTUtil.generateToken(claim);
+		userDetail.setToken(JWTUtil.generateToken(claim));
+
+		return userDetail;
 	}
 
 	private List<String> asList(String role) {
@@ -74,20 +74,29 @@ public class AuthServiceImpl implements AuthService {
 	}
 
 	@Override
-	public String login(String username, String password) {
+	public SecurityUserDetail login(UserInfo userInfo) {
 		// UsernamePasswordAuthenticationToken upToken = new
 		// UsernamePasswordAuthenticationToken(username, password);
 		// Authentication authentication =
 		// authenticationManager.authenticate(upToken);
 		// SecurityContextHolder.getContext().setAuthentication(authentication);
-		UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+		userInfo = userMapper.getUserForLogin(userInfo);
+
+		if (userInfo.getUserId() == null || "".equals(userInfo.getUserId())) {
+			return null;
+		}
 
 		Map<String, Object> claim = new HashMap<String, Object>();
-		claim.put(JWTUtil.PASSWORD, userDetails.getPassword());
-		claim.put(JWTUtil.USER_NAME, userDetails.getUsername());
+		claim.put(JWTUtil.PASSWORD, userInfo.getPassword());
+		claim.put(JWTUtil.USER_NAME, userInfo.getUserName());
+		userInfo.setAuthorities(asList("ROLE_USER"));
 
-		return JWTUtil.generateToken(claim);
+		SecurityUserDetail userDetail = SecurityUserFactory.create(userInfo);
 
+		userDetail.setToken(JWTUtil.generateToken(claim));
+
+		return userDetail;
 	}
 
 	@Override
