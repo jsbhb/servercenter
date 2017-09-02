a11a224d0c1d91b5edfd8598623ed734d6f26588
jsbhb <306494983@qq.com>
2017-09-02 13:16:21 +0800

    commit platUserType

---
+++ authserver/src/main/java/com/zm/auth/model/PlatUserType.java
@@ -0,0 +1,45 @@
+/**  
+ * Project Name:authserver  
+ * File Name:PlatUserType.java  
+ * Package Name:com.zm.gateway.model  
+ * Date:Aug 30, 20174:41:04 PM  
+ *  
+ */
+package com.zm.auth.model;
+
+/**
+ * ClassName: PlatUserType <br/>
+ * Function: TODO ADD FUNCTION. <br/>
+ * date: Aug 30, 2017 4:41:04 PM <br/>
+ * 
+ * @author hebin
+ * @version
+ * @since JDK 1.7
+ */
+public enum PlatUserType {
+	CROSS_BORDER("跨境业务", 1), EARA("区域中心", 2), BIG_TRADE("大贸", 3), DISTRIBUTION("分销", 4), CONSUMER("消费者", 5);
+
+	private String name;
+	private int index;
+
+	private PlatUserType(String name, int index) {
+		this.name = name;
+		this.index = index;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getIndex() {
+		return index;
+	}
+
+	public void setIndex(int index) {
+		this.index = index;
+	}
+}
