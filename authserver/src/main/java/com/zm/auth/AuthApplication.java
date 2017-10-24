f58f624b0703e6d7e486d468c7688b4fe6a5acb8
jsbhb <306494983@qq.com>
2017-10-24 16:17:13 +0800

    commit file

--- authserver/src/main/java/com/zm/auth/AuthApplication.java
+++ authserver/src/main/java/com/zm/auth/AuthApplication.java
@@ -3,12 +3,6 @@ package com.zm.auth;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.domain.AuditorAware;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 /**
  * 
