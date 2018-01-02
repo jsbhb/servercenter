47d490dec8b92cb8bf1ad45d6a258f7378bf6ff1
luciferwqy <190873411@163.com>
2018-01-02 22:10:04 +0800

    commit

---
+++ 3rdcenter/src/main/java/com/zm/thirdcenter/utils/SignUtil.java
@@ -0,0 +1,74 @@
+package com.zm.thirdcenter.utils;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class SignUtil {
+
+	/**
+	 * 获得分享链接的签名。
+	 * 
+	 * @param ticket
+	 * @param nonceStr
+	 * @param timeStamp
+	 * @param url
+	 * @return
+	 * @throws Exception
+	 */
+	public static String getSignature(String ticket, String nonceStr, String timeStamp, String url) {
+		String sKey = "jsapi_ticket=" + ticket + "&noncestr=" + nonceStr + "&timestamp=" + timeStamp + "&url=" + url;
+		System.out.println(sKey);
+		return getSignature(sKey);
+	}
+
+	/**
+	 * 验证签名。
+	 * 
+	 * @param signature
+	 * @param timestamp
+	 * @param nonce
+	 * @return
+	 */
+	public static String getSignature(String sKey) {
+		String ciphertext = null;
+		try {
+			MessageDigest md = MessageDigest.getInstance("SHA-1");
+			byte[] digest = md.digest(sKey.toString().getBytes());
+			ciphertext = byteToStr(digest);
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return ciphertext.toLowerCase();
+	}
+
+	/**
+	 * 将字节数组转换为十六进制字符串
+	 * 
+	 * @param byteArray
+	 * @return
+	 */
+	private static String byteToStr(byte[] byteArray) {
+		String strDigest = "";
+		for (int i = 0; i < byteArray.length; i++) {
+			strDigest += byteToHexStr(byteArray[i]);
+		}
+		return strDigest;
+	}
+
+	/**
+	 * 将字节转换为十六进制字符串
+	 * 
+	 * @param mByte
+	 * @return
+	 */
+	private static String byteToHexStr(byte mByte) {
+		char[] Digit = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+		char[] tempArr = new char[2];
+		tempArr[0] = Digit[(mByte >>> 4) & 0X0F];
+		tempArr[1] = Digit[mByte & 0X0F];
+
+		String s = new String(tempArr);
+		return s;
+	}
+}
---
+++ 3rdcenter/src/main/java/com/zm/thirdcenter/wx/SnsAccessTokenApi.java
@@ -0,0 +1,74 @@
+
+package com.zm.thirdcenter.wx;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.Callable;
+
+import com.zm.thirdcenter.pojo.WXLoginConfig;
+import com.zm.thirdcenter.utils.HttpClientUtil;
+import com.zm.thirdcenter.utils.RetryUtils;
+
+/**  
+ * ClassName: SnsAccessTokenApi <br/>  
+ * Function: 获取token. <br/>   
+ * date: Aug 18, 2017 4:04:44 PM <br/>  
+ *  
+ * @author wqy  
+ * @version   
+ * @since JDK 1.7  
+ */
+public class SnsAccessTokenApi {
+	//授权token的url
+	private static String auth_url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid={appid}&secret={secret}&code={code}&grant_type=authorization_code";
+
+	//基础token的url
+	private static String AccessToken_url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential";
+	
+	/**
+	 * 通过code获取access_token
+	 *
+	 * @param code
+	 *            第一步获取的code参数
+	 * @param appId
+	 *            应用唯一标识
+	 * @param secret
+	 *            应用密钥AppSecret
+	 * @return SnsAccessToken
+	 */
+	public static SnsAccessToken getSnsAccessToken(String appId, String secret, String code) {
+		final String accessTokenUrl = auth_url.replace("{appid}", appId).replace("{secret}", secret).replace("{code}", code == null?"":code);
+
+		return RetryUtils.retryOnException(3, new Callable<SnsAccessToken>() {
+
+			@Override
+			public SnsAccessToken call() throws Exception {
+				String json = HttpClientUtil.get(accessTokenUrl,null);
+				return new SnsAccessToken(json);
+			}
+		});
+	}
+
+	 /**
+     * 无条件强制更新 access token 值，不再对 cache 中的 token 进行判断
+     */
+    public static SnsAccessToken refreshAccessToken(WXLoginConfig ac) {
+    	String appId = ac.getAppId();
+		String appSecret = ac.getSecret();
+		final Map<String, String> queryParas = new HashMap<String, String>();
+		queryParas.put("secret", appSecret);
+		queryParas.put("appid", appId);
+
+        // 最多三次请求
+        SnsAccessToken result = RetryUtils.retryOnException(3, new Callable<SnsAccessToken>() {
+
+            @Override
+            public SnsAccessToken call() throws Exception {
+                String json = HttpClientUtil.get(AccessToken_url, queryParas);
+                return new SnsAccessToken(json);
+            }
+        });
+
+        return result;
+    }
+}
