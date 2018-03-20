ff202330cd677747baf60773a7a9d3f30609986f
luciferwqy <190873411@163.com>
2018-03-20 17:18:03 +0800

    commit

--- financecenter/src/main/java/com/zm/finance/pojo/card/Card.java
+++ financecenter/src/main/java/com/zm/finance/pojo/card/Card.java
@@ -9,7 +9,7 @@ public class Card  extends Pagination{
 	private String cardBank;
 	private String cardNo;
 	private String cardName;
-	private String cardPhone;
+	private String cardMobile;
 	private String remark;
 	private String createTime;
 	private String updateTime;
@@ -51,11 +51,11 @@ public class Card  extends Pagination{
 	public void setCardName(String cardName) {
 		this.cardName = cardName;
 	}
-	public String getCardPhone() {
-		return cardPhone;
+	public String getCardMobile() {
+		return cardMobile;
 	}
-	public void setCardPhone(String cardPhone) {
-		this.cardPhone = cardPhone;
+	public void setCardMobile(String cardMobile) {
+		this.cardMobile = cardMobile;
 	}
 	public String getRemark() {
 		return remark;
