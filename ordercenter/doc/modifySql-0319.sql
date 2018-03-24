fd84bcc844fba38d5f5ceb19d5e564ceb4d12ea6
luciferwqy <190873411@163.com>
2018-03-24 23:35:35 +0800

    commit

--- ordercenter/doc/modifySql-0319.sql
+++ ordercenter/doc/modifySql-0319.sql
@@ -1,2 +1,2 @@
-alter table order_goods add goods_id varchar(50) not null comment '商品ID';
+alter table order_goods add goods_id varchar(50) null comment '商品ID';
 alter table order_base add rebate tinyint unsigned default 0 comment '是否已经计算返佣，0未计算，1已计算';
