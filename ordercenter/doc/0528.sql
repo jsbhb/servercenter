8b24dc245ada42a5948cd7aa2f6ea82e48f3faa0
luciferwqy <190873411@163.com>
2018-06-01 20:54:40 +0800

    commit

--- ordercenter/doc/0528.sql
+++ ordercenter/doc/0528.sql
@@ -1,2 +1,2 @@
-alter table order_base add is_manual tinyint unsigned default 0 comment '是否手工单：0否，1是';
+alter table order_base add is_manual tinyint unsigned not null default 0 comment '是否手工单：0否，1是';
 alter table order_base add index is_manual(is_manual);
