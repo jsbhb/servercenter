64ce3e526bf62c64e97fd8060bcea2fe97095cf4
luciferwqy <190873411@163.com>
2018-06-08 11:32:41 +0800

    commit

--- goodscenter/doc/0608.sql
+++ goodscenter/doc/0608.sql
@@ -1,2 +1,4 @@
 alter table goods add column is_del tinyint unsigned default 0 comment '是否删除，0否；1是';
 alter table goods_item add column is_del tinyint unsigned default 0 comment '是否删除，0否；1是';
+alter table goods add index is_del(is_del);
+alter table goods_item add index is_del(is_del);
