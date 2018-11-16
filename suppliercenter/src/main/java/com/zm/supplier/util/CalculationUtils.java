14869f29ed8d5de426666bb99c1661ae4b996579
luciferwqy <190873411@163.com>
2018-11-16 10:32:24 +0800

    commit

--- suppliercenter/src/main/java/com/zm/supplier/util/CalculationUtils.java
+++ suppliercenter/src/main/java/com/zm/supplier/util/CalculationUtils.java
@@ -13,15 +13,15 @@ public class CalculationUtils {
 	 * @return 两个参数的和
 	 */
 	public static double add(double value1, double value2) {
-		BigDecimal b1 = new BigDecimal(Double.valueOf(value1));
-		BigDecimal b2 = new BigDecimal(Double.valueOf(value2));
+		BigDecimal b1 = new BigDecimal(String.valueOf(value1));
+		BigDecimal b2 = new BigDecimal(String.valueOf(value2));
 		return b1.add(b2).doubleValue();
 	}
 	
 	public static double add(double value1, double value2, double value3) {
-		BigDecimal b1 = new BigDecimal(Double.valueOf(value1));
-		BigDecimal b2 = new BigDecimal(Double.valueOf(value2));
-		BigDecimal b3 = new BigDecimal(Double.valueOf(value3));
+		BigDecimal b1 = new BigDecimal(String.valueOf(value1));
+		BigDecimal b2 = new BigDecimal(String.valueOf(value2));
+		BigDecimal b3 = new BigDecimal(String.valueOf(value3));
 		return b1.add(b2).add(b3).doubleValue();
 	}
 
@@ -35,8 +35,8 @@ public class CalculationUtils {
 	 * @return 两个参数的差
 	 */
 	public static double sub(double value1, double value2) {
-		BigDecimal b1 = new BigDecimal(Double.valueOf(value1));
-		BigDecimal b2 = new BigDecimal(Double.valueOf(value2));
+		BigDecimal b1 = new BigDecimal(String.valueOf(value1));
+		BigDecimal b2 = new BigDecimal(String.valueOf(value2));
 		return b1.subtract(b2).doubleValue();
 	}
 
@@ -50,8 +50,8 @@ public class CalculationUtils {
 	 * @return 两个参数的积
 	 */
 	public static double mul(double value1, double value2) {
-		BigDecimal b1 = new BigDecimal(Double.valueOf(value1));
-		BigDecimal b2 = new BigDecimal(Double.valueOf(value2));
+		BigDecimal b1 = new BigDecimal(String.valueOf(value1));
+		BigDecimal b2 = new BigDecimal(String.valueOf(value2));
 		return b1.multiply(b2).doubleValue();
 	}
 
@@ -72,8 +72,8 @@ public class CalculationUtils {
 		if (scale < 0) {
 			throw new IllegalAccessException("精确度不能小于0");
 		}
-		BigDecimal b1 = new BigDecimal(Double.valueOf(value1));
-		BigDecimal b2 = new BigDecimal(Double.valueOf(value2));
+		BigDecimal b1 = new BigDecimal(String.valueOf(value1));
+		BigDecimal b2 = new BigDecimal(String.valueOf(value2));
 		return b1.divide(b2, scale).doubleValue();
 	}
 
@@ -87,8 +87,8 @@ public class CalculationUtils {
 	 * @return 两个参数的商
 	 */
 	public static double div(double value1, double value2) {
-		BigDecimal b1 = new BigDecimal(Double.valueOf(value1));
-		BigDecimal b2 = new BigDecimal(Double.valueOf(value2));
+		BigDecimal b1 = new BigDecimal(String.valueOf(value1));
+		BigDecimal b2 = new BigDecimal(String.valueOf(value2));
 		return b1.divide(b2).doubleValue();
 	}
 }
