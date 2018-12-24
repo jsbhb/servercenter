6e791793bf26100903c1ad382553a98dc2fd838f
zhumenggzs2017why <281388429@qq.com>
2018-12-24 18:40:44 +0800

    commit

--- goodscenter/src/main/java/com/zm/goods/activity/backmanger/model/BargainActivityModel.java
+++ goodscenter/src/main/java/com/zm/goods/activity/backmanger/model/BargainActivityModel.java
@@ -6,6 +6,24 @@ import java.util.List;
 public class BargainActivityModel extends BaseActivityModel {
 
 	private List<BargainActivityGoodsModel> itemList;
+	private Integer joinPerson;
+	private String buyFlg;
+
+	public Integer getJoinPerson() {
+		return joinPerson;
+	}
+
+	public void setJoinPerson(Integer joinPerson) {
+		this.joinPerson = joinPerson;
+	}
+
+	public String getBuyFlg() {
+		return buyFlg;
+	}
+
+	public void setBuyFlg(String buyFlg) {
+		this.buyFlg = buyFlg;
+	}
 
 	public List<BargainActivityGoodsModel> getItemList() {
 		return itemList;
