a683fea07fbc0a27b7e499ea756c75ec9337d7a5
zhumenggzs2017why <281388429@qq.com>
2019-01-15 14:39:19 +0800

    commit 佳贝艾特

--- suppliercenter/src/main/java/com/zm/supplier/bussiness/controller/SupplierCallbackController.java
+++ suppliercenter/src/main/java/com/zm/supplier/bussiness/controller/SupplierCallbackController.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.zm.supplier.bussiness.service.SupplierCallbackService;
@@ -20,7 +21,7 @@ public class SupplierCallbackController {
 	@Resource
 	SupplierCallbackService supplierCallbackService;
 
-	@RequestMapping(value = "auth/{version}/order_status")
+	@RequestMapping(value = "auth/{version}/order_status", method = RequestMethod.POST)
 	public ResultModel orderStatusCallBack(@PathVariable("version") Double version,HttpServletRequest req,
 			@RequestBody OrderStatusCallBack statusCallBack) {
 		if (Constants.FIRST_VERSION.equals(version)) {
